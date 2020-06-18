258f90a9e6ff365168dbd59397846ef2d3f5c970
huancnttuet <huancnttuet@gmail.com>
2020-06-18 17:11:39 +0700

    @@

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -8,6 +8,6 @@ MYSQL_DBNAME = os.getenv('MYSQL_DBNAME')
 
 def init_app(app):
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://{}:{}@{}/{}'.format(
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
         MYSQL_USERNAME, MYSQL_PWD, MYSQL_HOST, MYSQL_DBNAME)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
