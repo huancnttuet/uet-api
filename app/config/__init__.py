10b22ac2afac0551597c23f48d97787fcd0caaa8
huancnttuet <huancnttuet@gmail.com>
2020-06-18 17:26:18 +0700

    ^^

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -7,7 +7,8 @@ MYSQL_DBNAME = os.getenv('MYSQL_DBNAME')
 
 
 def init_app(app):
-    print(MYSQL_HOST)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
-        MYSQL_USERNAME, MYSQL_PWD, MYSQL_HOST, MYSQL_DBNAME)
+    CLEARDB_DATABASE_URL = 'mysql://bd474dc8c06660:2c9f6312@us-cdbr-east-05.cleardb.net/heroku_e3a89c45e672a7c'
+    app.config['SQLALCHEMY_DATABASE_URI'] = CLEARDB_DATABASE_URL
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}'.format(
+    #     MYSQL_USERNAME, MYSQL_PWD, MYSQL_HOST, MYSQL_DBNAME)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
